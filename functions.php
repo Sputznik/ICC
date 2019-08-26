db261c8e188522ca59ff1b897503f8187852136b
unknown <samvthom16@gmail.com>
2019-08-26 16:41:04 +0530

    Enqueue after single css

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 define( 'ICC_VERSION', time() );
 
 add_action('wp_enqueue_scripts',function(){
-  wp_enqueue_style( 'icc', get_stylesheet_directory_uri().'/assets/css/style.css', array('sp-core-style'), ICC_VERSION );
+  wp_enqueue_style( 'icc', get_stylesheet_directory_uri().'/assets/css/style.css', array( 'sp-core-style', 'sp-core-single' ), ICC_VERSION );
 
 });
 
