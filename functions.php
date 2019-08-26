1022f49f74f3d3fdd9ec66262e67484bac5c770b
unknown <samvthom16@gmail.com>
2019-08-26 16:25:01 +0530

    Update css

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'ICC_VERSION', '1.0.1' );
+define( 'ICC_VERSION', time() );
 
 add_action('wp_enqueue_scripts',function(){
   wp_enqueue_style( 'icc', get_stylesheet_directory_uri().'/assets/css/style.css', array('sp-core-style'), ICC_VERSION );
