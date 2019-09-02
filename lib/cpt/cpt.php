fae7a10c5adfb560145dafda11dde0f10d24a1c5
unknown <samvthom16@gmail.com>
2019-09-02 11:14:54 +0530

    Lets Brew CPT

--- lib/cpt/cpt.php
+++ lib/cpt/cpt.php
@@ -14,6 +14,18 @@ add_filter( 'orbit_post_type_vars', function( $post_types ){
 		'supports'	=> array( 'title', 'editor', 'author', 'thumbnail', 'excerpt' )
 	);
 
+	$post_types['lets-brew'] = array(
+		'slug' 		=> 'lets-brew',
+		'labels'	=> array(
+			'name' 					=> 'Lets Brew',
+			'singular_name' => 'Brew',
+		),
+		//'rewrite'		=> array('slug' => 'incidents', 'with_front' => false ),
+		'menu_icon'	=> 'dashicons-format-video',
+		'public'		=> true,
+		'supports'	=> array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+	);
+
 
 	return $post_types;
 } );
