28cf6e1d15f1f635eb34b11bdc15ad806a941f0d
unknown <samvthom16@gmail.com>
2019-09-02 13:49:01 +0530

    Search Results template

--- search.php
+++ search.php
@@ -14,26 +14,28 @@
     ?>
   </div>
   <?php endif;?>
-  <div class="container">
-		<div class="row">
-			<div class="col-lg-12">
-				<?php if ( have_posts() ) : ?>
-        <h4 class='overlay-label'>Articles</h4>
-        <ul class="list-unstyled sp-icc-posts-3 icc-fixed">
-          <?php while ( have_posts() ) : the_post(); ?>
-				  <li class="sp-post">
-            <?php get_template_part( 'partials/post', 'common'); ?>
-				  </li>
-				  <?php endwhile;?>
-        </ul>
-				<?php
-					else :
-			 			printf( __('Sorry, but nothing matched your search terms. Please try again with some different keywords.') );
-					endif;
-				?>
-			</div>
-		</div>
-	</div>
+  <div class="articles-results">
+    <div class="container">
+  		<div class="row">
+  			<div class="col-lg-12">
+  				<?php if ( have_posts() ) : ?>
+          <h4 class='overlay-label'>Articles</h4>
+          <ul class="list-unstyled sp-icc-posts-3 icc-fixed">
+            <?php while ( have_posts() ) : the_post(); ?>
+  				  <li class="sp-post">
+              <?php get_template_part( 'partials/post', 'common'); ?>
+  				  </li>
+  				  <?php endwhile;?>
+          </ul>
+  				<?php
+  					else :
+  			 			printf( __('Sorry, but nothing matched your search terms. Please try again with some different keywords.') );
+  					endif;
+  				?>
+  			</div>
+  		</div>
+  	</div>
+  </div>
   <?php if ( have_posts() ): ?>
 	<!-- Previous/next page navigation. -->
 	<div class="container-fluid search-pagination">
@@ -57,6 +59,11 @@
   .term-results{
     /*background: #333;*/
     padding: 50px 15px;
+  }
+
+  .articles-results{
+    padding: 50px 0;
+    background: #ddd;
   }
 
   .overlay-label{
