cdb4ed9f699c2d1785013d21254dd427ce9c16c3
unknown <samvthom16@gmail.com>
2019-09-02 14:02:43 +0530

    Searchform

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php bloginfo('url');?>">
   <div class="input-group add-on">
-    <input class="form-control" placeholder="Search for laws and publications" value="" name="s" id="s" type="text">
+    <input style="padding:20px 15px;" class="form-control" placeholder="Search for cafes, city guides or articles" name="s" id="s" type="text" value="<?php _e( get_search_query() );?>" />
     <div class="input-group-btn">
       <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
     </div>
