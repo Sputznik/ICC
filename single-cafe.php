b9af68519c44cd9c13878bef425c1927f688084e
unknown <samvthom16@gmail.com>
2019-09-02 12:04:57 +0530

    Single template for cafe

--- single-cafe.php
+++ single-cafe.php
@@ -9,13 +9,15 @@
     <div class="row">
       <div class="col-sm-3">
         <h4><i class="fa fa-map-marker"></i>&nbsp;Address</h4>
+        <?php _e( do_shortcode( '[orbit_cf id="cafe-address"]' ) );?>
       </div>
       <div class="col-sm-3">
         <h4><i class="fa fa-clock-o"></i>&nbsp;Timings</h4>
+        <?php _e( do_shortcode( '[orbit_cf id="timings"]' ) );?>
       </div>
       <div class="col-sm-6">
         <div class="cafe-map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31108.637134282417!2d77.629565!3d12.934717!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3b9120fad40e4ffc!2sThird%20Wave%20Coffee%20Roasters%20%7C%20Koramangala!5e0!3m2!1sen!2sin!4v1567405702786!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+          <?php _e( do_shortcode( '[orbit_cf id="cafe-map"]' ) );?>
         </div>
       </div>
     </div>
