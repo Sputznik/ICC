54e430a7fe7477d265fd31115eface80406d18b7
unknown <samvthom16@gmail.com>
2019-09-02 11:28:34 +0530

    CPT and metaboxes

--- lib/cpt/cpt.php
+++ lib/cpt/cpt.php
@@ -2,13 +2,23 @@
 
 add_filter( 'orbit_post_type_vars', function( $post_types ){
 
+	$post_types['coffee'] = array(
+		'slug' 		=> 'coffee',
+		'labels'	=> array(
+			'name' 					=> 'Coffee',
+			'singular_name' => 'Coffee',
+		),
+		'menu_icon'	=> 'dashicons-art',
+		'public'		=> true,
+		'supports'	=> array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+	);
+
 	$post_types['interviews'] = array(
 		'slug' 		=> 'interviews',
 		'labels'	=> array(
 			'name' 					=> 'Interviews',
 			'singular_name' => 'Interview',
 		),
-		//'rewrite'		=> array('slug' => 'incidents', 'with_front' => false ),
 		'menu_icon'	=> 'dashicons-format-quote',
 		'public'		=> true,
 		'supports'	=> array( 'title', 'editor', 'author', 'thumbnail', 'excerpt' )
@@ -20,8 +30,18 @@ add_filter( 'orbit_post_type_vars', function( $post_types ){
 			'name' 					=> 'Lets Brew',
 			'singular_name' => 'Brew',
 		),
-		//'rewrite'		=> array('slug' => 'incidents', 'with_front' => false ),
 		'menu_icon'	=> 'dashicons-format-video',
+		'public'		=> true,
+		'supports'	=> array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+	);
+
+	$post_types['cafe'] = array(
+		'slug' 		=> 'cafe',
+		'labels'	=> array(
+			'name' 					=> 'Cafe',
+			'singular_name' => 'Cafe',
+		),
+		'menu_icon'	=> 'dashicons-media-interactive',
 		'public'		=> true,
 		'supports'	=> array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
 	);
@@ -29,3 +49,42 @@ add_filter( 'orbit_post_type_vars', function( $post_types ){
 
 	return $post_types;
 } );
+
+/* PUSH INTO THE GLOBAL VARS OF ORBIT TAXNOMIES */
+add_filter( 'orbit_taxonomy_vars', function( $taxonomies ){
+
+	$taxonomies['coffee-cat']	= array(
+		'label'			=> 'Coffee Categories',
+		'slug' 			=> 'coffee-cat',
+		'post_types'	=> array( 'coffee' )
+	);
+
+	$taxonomies['locations']	= array(
+		'label'			=> 'Location',
+		'slug' 			=> 'locations',
+		'post_types'	=> array( 'cafe' )
+	);
+
+	return $taxonomies;
+
+} );
+
+add_filter( 'orbit_meta_box_vars', function( $meta_box ){
+	$meta_box['cafe'] = array(
+		array(
+			'id'			=> 'cafe-meta-fields',
+			'title'		=> 'Additional Fields',
+			'fields'	=> array(
+				'timings' => array(
+					'type' => 'textarea',
+					'text' => 'Timings'
+				),
+				'cafe-address' => array(
+					'type' => 'textarea',
+					'text' => 'Address'
+				),
+			)
+		)
+	);
+	return $meta_box;
+});
