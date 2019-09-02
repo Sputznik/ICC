5b4cc2d5b00cb0236440fa01ce7beea8fc11c447
unknown <samvthom16@gmail.com>
2019-09-02 15:02:53 +0530

    Refactor taxonomy locations template

--- functions.php
+++ functions.php
@@ -13,3 +13,21 @@ function icc_excerpt_length( $length ) {
       return 30;
 }
 add_filter( 'excerpt_length', 'icc_excerpt_length', 999 );
+
+/*
+add_shortcode( 'icc_city_guides', function(){
+
+  $terms = get_terms( array(
+    'taxonomy' => 'locations',
+    'hide_empty' => false,
+  ) );
+
+  if( count( $terms ) ){
+
+  }
+  foreach( $terms as $term ){
+
+  }
+
+} );
+*/
--- taxonomy-locations.php
+++ taxonomy-locations.php
@@ -9,19 +9,12 @@
 <div class="container">
   <div class="row">
     <div class="col-sm-12">
-      <ul class='orbit-three-grid' style='margin:50px 0; padding-left: 0;'>
+      <ul class="list-unstyled sp-icc-posts-3 icc-grid">
         <?php while ( have_posts() ) : the_post(); ?>
-        <li class="orbit-article-db orbit-list-db">
-          <?php
-            global $post;
-            $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' );
-          ?>
-          <div class='orbit-thumbnail-bg' style='background-image: url( "<?php _e( $thumbnail[0] );?> ");position: relative;'>
-            <h4><?php the_title();?></h4>
-          	<a href='<?php the_permalink();?>'></a>
-          </div>
+        <li class="sp-post">
+          <?php get_template_part( 'partials/post', 'img-grid'); ?>
         </li>
-        <?php endwhile; ?>
+        <?php endwhile;?>
       </ul>
     </div>
   </div>
@@ -56,27 +49,5 @@
   line-height: 1.4;
   text-transform: uppercase;
   max-width: 500px;
-}
-.orbit-thumbnail-bg{
-  position: relative;
-}
-.orbit-thumbnail-bg h4{
-  position: absolute;
-  transform: translate( -50%, -50% );
-  left: 50%;
-  top: 50%;
-  margin-top: 0;
-  color: #fff;
-  z-index: 2;
-  width: 80%;
-  text-align: center;
-}
-.orbit-thumbnail-bg a[href]{
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: #33333399;
 }
 </style>
