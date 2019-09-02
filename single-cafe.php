4fb2c6724dae2ec9c7131bea3bf46cbc3e83b166
unknown <samvthom16@gmail.com>
2019-09-02 11:50:36 +0530

    Single template for cafe

--- single-cafe.php
+++ single-cafe.php
@@ -1,12 +1,17 @@
 <?php get_header();?>
 <?php if( have_posts() ): while( have_posts() ): the_post();?>
   <div class="cafe-title">
-    <h2><?php the_title();?><h2>
+    <div class="container">
+      <h2><?php the_title();?></h2>
+    </div>
   </div>
 <?php endwhile; endif; ?>
 <?php get_footer();?>
 
 <style>
+  @media( min-width: 768px ){
+    .cafe-title{ margin-top: 80px; }
+  }
   .cafe-title{
     background: #333;
     padding: 120px 0 80px;
