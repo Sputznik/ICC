bf0c50bbb8f43e933a7dddc593be3034b7d8d1b0
unknown <samvthom16@gmail.com>
2019-09-02 12:32:10 +0530

    Locations template

--- taxonomy-locations.php
+++ taxonomy-locations.php
@@ -5,6 +5,21 @@
     <h2><?php _e( $term->name );?></h2>
   </div>
 </div>
+<?php if ( have_posts() ) : ?>
+<ul class='orbit-three-grid' style='margin-bottom:50px; padding-left: 0;'>
+  <?php while ( have_posts() ) : the_post(); ?>
+  <li class="orbit-article-db orbit-list-db">
+    <?php
+      global $post;
+      $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' );
+    ?>
+    <div class='orbit-thumbnail-bg' style='background-image: url( "<?php _e( $thumbnail[0] );?> ");position: relative;'>
+    	<a href='<?php the_permalink();?>' style="position: absolute; top:0;left:0;width:100%;height: 100%;"></a>
+    </div>
+  </li>
+  <?php endwhile; ?>
+</ul>
+<?php endif; ?>
 <?php get_footer();?>
 <style>
 @media( min-width: 768px ){
