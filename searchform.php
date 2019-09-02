b0ac2bb29ed627b394448882ed11a2590ef29a48
unknown <samvthom16@gmail.com>
2019-09-02 13:59:23 +0530

    Searchform

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<form role="search" method="get" id="searchform" action="<?php bloginfo('url');?>">
+  <div class="input-group add-on">
+    <input class="form-control" placeholder="Search for laws and publications" value="" name="s" id="s" type="text">
+    <div class="input-group-btn">
+      <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+    </div>
+  </div>
+</form>
