890505e64e6b9db49c58bf2cd69e852129b4aba0
unknown <samvthom16@gmail.com>
2019-09-02 15:45:32 +0530

    Test ICC City guides

--- functions.php
+++ functions.php
@@ -29,7 +29,7 @@ add_shortcode( 'icc_city_guides', function(){
 
       $image_url = apply_filters( 'taxonomy-images-queried-term-image-url', '' );
 
-      echo $image_url;
+      print_r( $term );
 
       ?>
       <div class="bg-img-icc" style="background-image: url( <?php _e( $image_url );?> );">
