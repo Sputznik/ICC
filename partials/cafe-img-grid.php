85a3a3c07c98391721c87949be3ae4e4dc42186d
unknown <samvthom16@gmail.com>
2019-09-03 11:28:31 +0530

    Test

--- partials/cafe-img-grid.php
+++ partials/cafe-img-grid.php
@@ -1,7 +1,8 @@
-<?php //$image_url = icc_get_taxonomy_image_url( $term );?>
+<?php //$image_url = icc_get_taxonomy_image_url( $term ); ?>
+<?php $image_url='';?>
 <div class="bg-img-icc" style="background-image: url( <?php _e( $image_url );?> );">
   <div class="sp-post-desc">
-    <h3><?php _e( $term->name );?></h3>
+    <h3><?php //_e( $term->name );?></h3>
   </div>
-  <a class="icc-img-link" href="<?php _e( get_term_link( $term ) );?>"></a>
+  <a class="icc-img-link" href="<?php _e( //get_term_link( $term ) );?>"></a>
 </div>
